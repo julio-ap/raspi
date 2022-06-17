93d0805dbf75c1004c89765ccbe4e80136af7af7
raspi <julioap5428@gmail.com>
2022-06-16 21:03:01 -0400

    firebase agregado

--- readserial.py
+++ readserial.py
@@ -2,6 +2,7 @@ import serial
 arduino= serial.Serial(port= "/dev/ttyAMA0", baudrate=9600, timeout=.1)
 
 while True:
-    if (arduino.in_waiting >0):
-         line=arduino.readline()
-         print(line.decode('utf-8'))
+
+     if (arduino.in_waiting >0):
+        line=arduino.readline()
+        print(line.decode('utf-8'))
--- test_fire.py
+++ test_fire.py
@@ -5,19 +5,19 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 
-cred = credentials.Certificate('/home/pi/cred.json')
+cred = credentials.Certificate('/home/pi/Desktop/projects/cred.json')
 
 
 # Initialize the app with a service account, granting admin privileges
 
 firebase_admin.initialize_app(cred, {
 
-    'databaseURL': 'https://tu-proyecto.firebaseio.com/'
+    'databaseURL': 'https://p-rasp-default-rtdb.firebaseio.com/'
 
 })
 
 
-ref = db.reference('demo')
+ref = db.reference('message')
 
 print(ref.get())
 
